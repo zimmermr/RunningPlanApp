8a3d05991d27b394898a35cf378b4a9a62cc1a0b
Michael Zimmerman <65474223+zimmermr@users.noreply.github.com>
2022-10-08 20:38:06 -0400

    Add files via upload

---
+++ Default.aspx
@@ -0,0 +1,62 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="RunCalculator.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <h1>Run Calculator</h1>
+    <form id="form1" runat="server">
+        <div>
+            <p>Enter a recent finish time for a race you completed:</p>
+            <asp:Label ID="Label1" runat="server" Text="What distance did you run?"></asp:Label>
+            <asp:DropDownList ID="lstRecentRace" runat="server">
+                <asp:ListItem Selected="True" ID="li5K" Text="5K" Value="5000"></asp:ListItem>
+                <asp:ListItem ID="li10K" Text="10K" Value="10000"></asp:ListItem>
+                <asp:ListItem ID="liHalf" Text="Half Marathon" Value="21097.5"></asp:ListItem>
+                <asp:ListItem ID="liMarathon" Text="Marathon" Value="42195"></asp:ListItem>
+            </asp:DropDownList>
+            <p>How many miles do you run per week?</p>
+            <asp:DropDownList ID="lstWeeklyMileage" runat="server">
+                <asp:ListItem Selected="True" ID="li0to10" Text="0-10 miles" Value="10"></asp:ListItem>
+                <asp:ListItem ID="li11to25" Text="11-25" Value="25"></asp:ListItem>
+                <asp:ListItem ID="li26to50" Text="26-50" Value="50"></asp:ListItem>
+                <asp:ListItem ID="liOver51" Text="51+" Value="51"></asp:ListItem>
+            </asp:DropDownList>
+            <p>What was your finish time?</p>
+            <asp:Table ID="Table1" runat="server">
+                <asp:TableRow runat="server">
+                    <asp:TableCell>
+                        <asp:Label ID="lblHours" runat="server" Text="Hours"></asp:Label>
+                    </asp:TableCell>
+                    <asp:TableCell>
+                        <asp:Label ID="lblMin" runat="server" Text="Minutes"></asp:Label>
+                    </asp:TableCell>
+                    <asp:TableCell>
+                        <asp:Label ID="lblSec" runat="server" Text="Seconds"></asp:Label>
+                    </asp:TableCell>
+                </asp:TableRow>
+                <asp:TableRow>
+                    <asp:TableCell>
+                        <asp:TextBox ID="txtHours" runat="server" MaxLength="2" Width="20" Text="00"></asp:TextBox>
+                    </asp:TableCell>
+                    <asp:TableCell>
+                        <asp:TextBox ID="txtMin" runat="server" MaxLength="2" Width="20" Text="00"></asp:TextBox>
+                    </asp:TableCell>
+                    <asp:TableCell>
+                        <asp:TextBox ID="txtSec" runat="server" MaxLength="2" Width="20" Text="00"></asp:TextBox>
+                    </asp:TableCell>
+                </asp:TableRow>
+            </asp:Table>
+        </div>
+        <asp:Button ID="btnCalcTimes" runat="server" Text="Estimate race times" OnClick="btnCalcTimes_Click" />
+        <div>
+            <asp:Label ID="lblTemp" runat="server" Text="Race time here"></asp:Label>
+            <asp:GridView ID="tblEstTime" runat="server" AutoGenerateColumns="true" BorderWidth="1" BorderColor="DarkSlateGray">
+            </asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace RunCalculator
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        DataTable tblEstTimes = new DataTable();
+        DataRow tblRow;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Calculate()
+        {
+            int previousTime = ConvertToSeconds();
+            string fiveKTime,
+                tenKTime,
+                halfTime,
+                marathonTime;
+            double previousDistance = 0.0;
+
+            switch (lstRecentRace.SelectedIndex)
+            {
+                case 0: previousDistance = 5000.0;
+                    break;
+                case 1: previousDistance = 10000.0;
+                    break;
+                case 2: previousDistance = 21097.5;
+                    break;
+                case 3: previousDistance = 42195;
+                    break;
+            }
+
+            fiveKTime = ConvertToTimeFormat(estimateFiveKTime(previousTime, previousDistance));
+            tenKTime = ConvertToTimeFormat(estimateTenKTime(previousTime, previousDistance));
+            halfTime = ConvertToTimeFormat(estimateHalfTime(previousTime, previousDistance));
+            marathonTime = ConvertToTimeFormat(estimateMarathonTime(previousTime, previousDistance));
+
+            lblTemp.Text = "5K time: " + fiveKTime + "<br />" +
+                "10K time: " + tenKTime + "<br />" +
+                "Half Marathon time: " + halfTime + "<br />" +
+                "Marathon time: " + marathonTime;
+        }
+
+        protected double riegelPower()
+        {
+            double riegelPower = 0.0;
+
+            switch (lstWeeklyMileage.SelectedIndex)
+            {
+                case 0:
+                    riegelPower = 1.2;
+                    break;
+                case 1:
+                    riegelPower = 1.15;
+                    break;
+                case 2:
+                    riegelPower = 1.1;
+                    break;
+                case 3:
+                    riegelPower = 1.06;
+                    break;
+            }
+
+            return riegelPower;
+        }
+
+        protected double estimateFiveKTime (int previousTime, double previousDistance)
+        {
+            double predictedTime = previousTime * Math.Pow((5000 / previousDistance), riegelPower());
+            return predictedTime;
+        }
+
+        protected double estimateTenKTime (int previousTime, double previousDistance)
+        {
+            double predictedTime = previousTime * Math.Pow((10000 / previousDistance), riegelPower());
+            return predictedTime;
+        }
+
+        protected double estimateHalfTime(int previousTime, double previousDistance)
+        {
+            double predictedTime = previousTime * Math.Pow((21097.5 / previousDistance), riegelPower());
+            return predictedTime;
+        }
+
+        protected double estimateMarathonTime(int previousTime, double previousDistance)
+        {
+            double predictedTime = previousTime * Math.Pow((42195 / previousDistance), riegelPower());
+            return predictedTime;
+        }
+
+        protected string ConvertToTimeFormat(double totalSeconds)
+        {
+            int convertedSeconds = (int)Math.Round(totalSeconds, 0);
+            int hours = convertedSeconds / 3600,
+                minutes = (convertedSeconds / 60) % 60,
+                seconds = convertedSeconds % 60;
+
+            string strHours, strMinutes, strSeconds;
+
+            if (hours == 0)
+            {
+                strHours = "";
+            }
+            else if (hours < 10)
+            {
+                strHours = "0" + hours.ToString();
+            }
+            else
+            {
+                strHours = hours.ToString();
+            }
+
+            if (hours == 0 && minutes == 0)
+            {
+                strMinutes = "";
+            }
+            else if (hours > 0 && minutes == 0)
+            {
+                strMinutes = "00";
+            }
+            else if (minutes < 10)
+            {
+                strMinutes = "0" + minutes.ToString();
+            }
+            else
+            {
+                strMinutes = minutes.ToString();
+            }
+
+            if (seconds == 0)
+            {
+                strSeconds = "00";
+            }
+            else if (seconds < 10)
+            {
+                strSeconds = "0" + seconds.ToString();
+            }
+            else
+            {
+                strSeconds = seconds.ToString();
+            }
+
+            if (hours == 0 && minutes == 0)
+            {
+                return strSeconds;
+            }
+            else if (hours == 0)
+            {
+                return strMinutes + ":" + strSeconds;
+            }
+            else
+            {
+                return strHours + ":" + strMinutes + ":" + strSeconds;
+            }
+        }
+
+        protected int ConvertToSeconds()
+        {
+            int hourSeconds = 3600 * int.Parse(txtHours.Text);
+            int minSeconds = 60 * int.Parse(txtMin.Text);
+            int seconds = int.Parse(txtSec.Text);
+            return hourSeconds + minSeconds + seconds;
+        }
+
+        protected void btnCalcTimes_Click(object sender, EventArgs e)
+        {
+            Calculate();
+        }
+    }
+}
